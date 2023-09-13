944517145c7fa893c2596471e99ceda7198d2a70
pavro_roman <roman.pavlov.1996@bk.ru>
2023-09-13 17:01:57 +0300

    Changed url

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const router = jsonServer.router("json-server/db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use("/", router);
+server.use("/api", router);
 server.listen(process.env.PORT || 5000, () => {
   console.log("JSON Server is running");
 });
